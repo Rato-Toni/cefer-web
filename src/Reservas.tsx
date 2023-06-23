a13ea61871cf58a0fef087f63d65907be22257af
AdaltonF <adaltonfilho2003@gmail.com>
2023-06-23 10:16:02 -0300

    Criando página de reservas

---
+++ src/Reservas.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Reservas.css";
+
+function Reservas() {
+    return (
+    <div className="reserva">
+        <h1>Reservas aqui</h1>
+    </div>
+  );
+}
+
+export default Reservas;
