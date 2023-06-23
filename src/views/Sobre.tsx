55e12d4deed067cda4d8b92b465eaf128fdd2326
JotaGHz <jota@Jota2>
2023-06-23 06:44:01 -0300

    Cria componente Sobre

---
+++ src/views/Sobre.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Sobre() {
+    // Teste
+    return (
+    <div>
+      <h1>Sobre o CEFER</h1>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Veniam voluptates temporibus adipisci corrupti. Itaque, 
+        dolor culpa. Dignissimos natus dolores vero modi corrupti 
+        provident. Magni explicabo atque at blanditiis commodi perspiciatis.
+        </p>
+    </div>
+  );
+}
+
+export default Sobre;
